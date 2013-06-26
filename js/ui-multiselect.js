5b4e0ed1bc5c1fc550e9d8900bc4690c2898fd6d
Ray Shan <ray@rayshan.com>
2013-06-25 23:49:31 -0500

    general cleanup

--- js/ui-multiselect.js
+++ js/ui-multiselect.js
@@ -20,39 +20,30 @@ angular.module('ui.multiselect', []).directive('uiMultiselect', [function () {
 
 			scope.query = "";
 			scope.limitFilter = attrs.limitFilter;
-
 			scope.width = attrs.width;
-
-			if (attrs.placeholder) {
-				scope.placeholder = attrs.placeholder;
-			} else {
-				scope.placeholder = '';
-			}
+			scope.placeholder = attrs.placeholder ? attrs.placeholder : '';
 
 			scope.$watch('query', function (newValue) {
 				var length = newValue.length > 0 ? newValue.length : scope.placeholder.length;
 				scope.inputWidth = scope.placeholder.length > 0 ? 10 + length * 6 : 100;
-			});
+			}); // expand input box width based on content
 
-			scope.addItem = function (item, position, $event) {
+			scope.addItem = function (item, position) {
 				scope.output.splice(position, 0, item); // adds before position
 				scope.query = [];
-				if ($event && $event.keyCode === 1) scope.hoverSelector = false; // mouse click hides selector
 			};
 			scope.removeItem = function (position) {
 				scope.output.splice(position, 1); // splice @ exact location
 			};
 
-			scope.focusChoice = []; // hide removeItem x button
+			scope.focusChoice = []; // clears focus on any chosen item for del
 
 			scope.focus = function () {
 				scope.focusInput = true;
-				scope.selectorPosition = undefined; // start @ first item
+				scope.selectorPosition = 0; // start @ first item
 			};
 			scope.blur = function () {
 				scope.focusInput = false;
-//				scope.query = [];
-//				scope.inputPosition = undefined; // blur happens first before addItem
 			};
 
 			scope.hoverSelector = false;
@@ -69,11 +60,13 @@ angular.module('ui.multiselect', []).directive('uiMultiselect', [function () {
 					13: 'enter',
 					9 : 'tab',
 					27: 'esc'
-				}; var key = keys[$event.keyCode];
+				};
+				var key = keys[$event.keyCode];
+				var queryIsEmpty = scope.query.length === 0;
 
-				if (!key || (key === 'backspace' && scope.query.length !== 0)) {
-					// backspace should work if !!query
-					scope.selectorPosition = undefined;
+				if (!key || (key === 'backspace' && !queryIsEmpty)) {
+					// backspace should work when query isn't empty
+					scope.selectorPosition = 0;
 				} else {
 					var atTop = scope.selectorPosition === 0;
 					var atBottom = scope.selectorPosition === scope.filteredData.length - 1;
@@ -85,26 +78,20 @@ angular.module('ui.multiselect', []).directive('uiMultiselect', [function () {
 							scope.selectorPosition = scope.filteredData.length - 1;
 						} else scope.selectorPosition--;
 					} else if (key === 'down') {
-						console.log(scope.selectorPosition);
-						if (!scope.selectorPosition && scope.selectorPosition != 0) {
-							scope.selectorPosition = 0;
-						} else if (atBottom) {
+						if (atBottom) {
 							scope.selectorPosition = 0;
 						} else scope.selectorPosition++;
 					} else if (key === 'backspace') {
 						if (choiceFocused) {
 							scope.removeItem(scope.output.length - 1);
 							scope.focusChoice = [];
-						} else { // !choiceFocused
-							scope.focusChoice[scope.output.length - 1] = true;
-						}
+						} else scope.focusChoice[scope.output.length - 1] = true;
 					} else if ((key === 'enter' || key === 'tab') && filteredDataExists) {
 						scope.addItem(scope.filteredData[scope.selectorPosition], scope.output.length);
 					} else if (key === 'esc' && !!scope.focusChoice) scope.focusChoice = [];
 
 					$event.preventDefault();
 				}
-//				console.log(key);
 			};
 
 		}
