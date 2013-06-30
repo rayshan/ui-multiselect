dc7b83043eb7ecce40b2dc6e91883204dac795fa
Ray Shan <ray@rayshan.com>
2013-06-30 14:08:35 -0500

    removed server scripts

--- scripts/web-server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect')
-  , http = require('http');
-
-var app = connect()
-  .use(connect.static('js'))
-  .use(connect.logger('dev'));
-  // concise output colored by response status for development use
-
-http.createServer(app).listen(8000);
